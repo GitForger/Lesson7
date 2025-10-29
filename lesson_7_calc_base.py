4763d30ba11085df4a272f216da526dd6858a874
GitForger <trueforgefather@gmail.com>
2025-10-29 15:49:47 +0700

    Update lesson_7_calc_base.py

--- lesson_7_calc_base.py
+++ lesson_7_calc_base.py
@@ -19,19 +19,17 @@ class CalculatorPage:
         self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
     
     def set_delay(self, delay_seconds):
-        """Устанавливает задержку вычислений"""
         delay_input = self.driver.find_element(*self.delay_field)
         delay_input.clear()
         delay_input.send_keys(str(delay_seconds))
     
     def click_button(self, button):
-        """Нажимает кнопку калькулятора"""
         button_locator = self.buttons[button]
         self.driver.find_element(*button_locator).click()
     
     def wait_for_result(self, expected_result, timeout=50):
-        """Ждет пока результат не станет равен expected_result"""
         WebDriverWait(self.driver, timeout).until(
             lambda driver: driver.find_element(*self.result_field).text == expected_result
         )
         print(f"✅ Результат {expected_result} отобразился!")
+
